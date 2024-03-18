23173064f762e7e4869364985570e4c5395d5a4f
Muhammad Shahzaib Minhas <mshahzaib1629@gmail.com>
2024-03-18 17:50:27 +0500

    users & todos modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { TodosModule } from './modules/todos/todos.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
